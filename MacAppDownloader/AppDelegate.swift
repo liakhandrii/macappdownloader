0d3e27f4ea41ef0753d6766ad595d7e6463bee50
Andrii Liakh <liakhandrii@gmail.com>
2018-07-10 13:12:52 +0300

    Initial Commit

---
+++ MacAppDownloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAppDownloader
+//
+//  Created by Andrew Liakh on 7/10/18.
+//  Copyright © 2018 Twopeople Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacAppDownloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAppDownloader
+//
+//  Created by Andrew Liakh on 7/10/18.
+//  Copyright © 2018 Twopeople Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
